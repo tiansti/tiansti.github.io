b510da0f4c625557c9fbfd91085104b939567464
tiansti <humbleoldman@hotmail.com>
2025-01-23 09:12:28 +0000

    更新博客 UTC 2025-01-23 Thursday 09:12:27 Updated By Github Actions

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["posts/7599.html","posts/4370.html","posts/10f9.html","posts/d25.html","posts/1a03.html","posts/9bba.html","posts/6e4.html","posts/cee1.html","posts/4f1b.html","posts/4522.html","posts/1748.html","posts/6c0f.html","posts/4d9c.html","posts/a9f4.html","posts/3fad.html","posts/7fcc.html","posts/6bf2.html","posts/b6a6.html","posts/5afa.html","posts/54dd.html","posts/b8bb.html","posts/55c8.html","posts/a6b5.html"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
